1780411c0fc3e5e671f1fe4333623885079520b5
Elizabeth Rosso <elizabeth.a.rosso@gmail.com>
2018-09-14 13:41:49 +0000

    CS50 Week1 Live

---
+++ helloworld.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+   // printf() displays the string inside quotation
+   printf("Hello, World!\n");
+
+   int x = 0, y = 0;
+
+   if(x<y)
+        printf("x is less than y\n");
+    else if(x>y)
+        printf("x is greater than y\n");
+    else if(x==y)
+        printf("x is equal to y\n");
+
+
+   return 0;
+}
